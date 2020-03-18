895f1fcbc090927c054e88c743de0c83fb8bf9dd
Jakub Nozderka <jaktotowiem@o2.pl>
2020-03-18 16:49:30 +0100

    Added getters to Node<T>

--- src/main/java/pl/sise/tree/Node.java
+++ src/main/java/pl/sise/tree/Node.java
@@ -19,4 +19,16 @@ public class Node<T> {
     public void addChild(T childLabel) {
         children.add(new Node<T>(childLabel, this));
     }
+
+    public T getLabel() {
+        return this.label;
+    }
+
+    public Node<T> getParent() {
+        return this.parent;
+    }
+
+    public ArrayList<Node<T>> getChildren() {
+        return this.children;
+    }
 }
